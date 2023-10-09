b00716b5206e7f25623e50ac2181d6fe90d19449
Rafael Silverio <rafael.silverio.s@outlook.com>
2023-10-09 15:54:27 -0300

    Imagem editada

--- src/Containers/Hero/index.tsx
+++ src/Containers/Hero/index.tsx
@@ -10,7 +10,7 @@ const Hero = ({ id }: HeroProps) => {
   return (
     <HeroEstilo id={id}>
       <Container>
-        <ImgBody scr="https://raw.githubusercontent.com/rafasilverios/EBAC-Engenheiro-Front-end/main/M%C3%B3dulo%2020%20-%20Construindo%20e-mails%20com%20HTML/Aulas/images/corpweb-logo.png" />
+        <ImgBody scr="https://raw.githubusercontent.com/rafasilverios/Site-Corpweb/main/src/img/print-lux.PNG?token=GHSAT0AAAAAACC3ZCEN25WS2HCRQ2B2DZ6WZJEJMEQ" />
       </Container>
     </HeroEstilo>
   )
