dbbb37e78ea1d587352c259eda7d39ccfb52965a
Rafael Silverio <rafael.silverio.s@outlook.com>
2023-10-24 16:40:23 -0300

    teste imagem sobre arquivo interno

--- src/Containers/Sobre/index.tsx
+++ src/Containers/Sobre/index.tsx
@@ -68,10 +68,7 @@ const Sobre = ({ id }: SobreProps) => {
               seu público.
             </Paragrafo>
           </ConfigDisplayTexto>
-          <ImgSobre
-            maxWidth="675px"
-            src="https://raw.githubusercontent.com/rafasilverios/EBAC-Engenheiro-Front-end/main/M%C3%B3dulo%2020%20-%20Construindo%20e-mails%20com%20HTML/Aulas/images/construindo-site.jpg"
-          />
+          <ImgSobre maxWidth="675px" src="../img/site para empresa.jpg" />
         </ContainerMaior>
       </SobreEstilo>
       <SobreEstilo display="block" justifyContent="flex-start">
