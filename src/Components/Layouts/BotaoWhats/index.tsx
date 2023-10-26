facc55994ec51ce0b268b1d42b686d08ccd741b5
Rafael Silverio <rafael.silverio.s@outlook.com>
2023-10-26 11:06:01 -0300

    botao whats com animação

--- src/Components/Layouts/BotaoWhats/index.tsx
+++ src/Components/Layouts/BotaoWhats/index.tsx
@@ -24,7 +24,7 @@ const BotaoWhats = () => {
   return (
     <BotaoWhatsEstilo
       className="BotaoWhatsEstilo"
-      href="https://wa.me/5541984424733?text=Ol%C3%A1%2C+tudo+bem%3F%0AQuero+mais+informações+sobre+os+planos+oferecidos+para+desenvolver+um+site+para+minha+empresa.+Seria+poss%C3%ADvel+fornecer+mais+informa%C3%C3µes+sobre+os+detalhes+dos+planos+e+como+eles+podem+beneficiar+o+meu+neg%C3%B3cio%3F"
+      href="https://wa.me/5541984424733?text=Ol%C3%A1%2C+tudo+bem%3F%0AQuero+mais+informações+sobre+os+planos+oferecidos+para+desenvolver+um+site+para+minha+empresa.+Seria+poss%C3%ADvel+fornecer+mais+informações+sobre+os+detalhes+dos+planos+e+como+eles+podem+beneficiar+o+meu+neg%C3%B3cio%3F"
       target="_blank"
       aria-label="Clique para falar com a CorpWeb pelo WhatsApp"
       rel="noreferrer"
